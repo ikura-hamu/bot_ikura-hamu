e6fb79050171931bebba0d65bde5444d9b1e8abb
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2023-08-09 16:03:27 +0900

    REST Client拡張機能を導入

--- src/conf/traq.go
+++ src/conf/traq.go
@@ -5,3 +5,7 @@ import "os"
 func GetTraqClientConf() (string, bool) {
 	return os.LookupEnv("BOT_ACCESS_TOKEN")
 }
+
+func GetBotToken() string {
+	return os.Getenv("BOT_VERIFICATION_TOKEN")
+}
--- src/router/middleware.go
+++ src/router/middleware.go
@@ -3,14 +3,16 @@ package router
 import (
 	"net/http"
 
+	"github.com/ikura-hamu/bot_ikura-hamu/src/conf"
 	"github.com/labstack/echo/v4"
 )
 
 func botVerifyMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		if len(c.Request().Header[http.CanonicalHeaderKey("X-TRAQ-BOT-TOKEN")]) == 0 ||
-			c.Request().Header[http.CanonicalHeaderKey("X-TRAQ-BOT-TOKEN")][0] != "" {
+		if len(c.Request().Header[http.CanonicalHeaderKey("X-TRAQ-BOT-TOKEN")]) == 0 {
 			return echo.NewHTTPError(http.StatusUnauthorized, "no bot token")
+		} else if c.Request().Header[http.CanonicalHeaderKey("X-TRAQ-BOT-TOKEN")][0] != conf.GetBotToken() {
+			return echo.NewHTTPError(http.StatusUnauthorized, "invalid token")
 		}
 		return next(c)
 	}
