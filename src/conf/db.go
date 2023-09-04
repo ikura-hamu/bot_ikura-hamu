925f7c84a082f4db4dba93d2cac340b9539a3b2a
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2023-09-05 00:01:46 +0900

    環境変数ミス

--- src/conf/db.go
+++ src/conf/db.go
@@ -10,7 +10,7 @@ func GetMongoUri() string {
 	hostName := getEnvOrDefault("NS_MONGODB_HOSTNAME", "db")
 	password := getEnvOrDefault("NS_MONGODB_PASSWORD", "password")
 	port := getEnvOrDefault("NS_MONGODB_PORT", "27017")
-	user := getEnvOrDefault("NS_MONGO_USER", "root")
+	user := getEnvOrDefault("NS_MONGODB_USER", "root")
 
 	// return fmt.Sprintf("mongodb://%s:%v@%s:%s/%s?authSource=admin", user, password, hostName, port, dbName)
 	uri := "mongodb://" + user + ":" + password + "@" + hostName + ":" + port + "/" + dbName + "?authSource=admin"
