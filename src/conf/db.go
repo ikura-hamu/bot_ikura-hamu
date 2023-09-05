5829cd952fa58e5972e5eee5eb2098d76804d687
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2023-09-05 18:28:56 +0900

    usernameとpasswordをクエリエスケープ

--- src/conf/db.go
+++ src/conf/db.go
@@ -2,6 +2,7 @@ package conf
 
 import (
 	"fmt"
+	"net/url"
 )
 
 type MongoDBConfig struct {
@@ -14,9 +15,9 @@ type MongoDBConfig struct {
 func GetMongoUri() *MongoDBConfig {
 	dbName := getEnvOrDefault("NS_MONGODB_DATABASE", "bot")
 	hostName := getEnvOrDefault("NS_MONGODB_HOSTNAME", "db")
-	password := getEnvOrDefault("NS_MONGODB_PASSWORD", "password")
+	password := url.QueryEscape(getEnvOrDefault("NS_MONGODB_PASSWORD", "password"))
 	port := getEnvOrDefault("NS_MONGODB_PORT", "27017")
-	user := getEnvOrDefault("NS_MONGODB_USER", "root")
+	user := url.QueryEscape(getEnvOrDefault("NS_MONGODB_USER", "root"))
 
 	return &MongoDBConfig{
 		Host:         fmt.Sprintf("%s:%s", hostName, port),
